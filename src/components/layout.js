dc0d0715b212422cdaefd4b31ca232e637cac594
Mahlatse Malete <mahlatse.malete@gviworld.com>
2021-10-07 10:42:58 +0200

    added hero

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Nav />
-      <div className="container mx-auto py-8 bg-green-500 sm:bg-blue-300 md:bg-red-500 ">
+      <div className="container mx-auto py-8">
         {children}
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,13 @@ import * as React from 'react'
 
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import Hero from '../components/hero'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <Seo title="Home" />
-      <title>Home</title>
-      <h1>This is the home page</h1>
+      <Hero />
     </Layout>
   )
 }
